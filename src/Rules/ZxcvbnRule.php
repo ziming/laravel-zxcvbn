c568a13c9dae8444367c18fccb7b641e16174e56
ziming <ziming.opensource@gmail.com>
2022-06-10 16:33:35 +0800

    Initial Zxcvbn rule

---
+++ src/Rules/ZxcvbnRule.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ziming\LaravelZxcvbn\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use ZxcvbnPhp\Zxcvbn;
+
+class ZxcvbnRule implements Rule
+{
+    private readonly Zxcvbn $zxcvbn;
+    private readonly int $minZxcvbnScore;
+
+    public function __construct(private readonly array $userInputs = [])
+    {
+        $this->zxcvbn = new Zxcvbn();
+        $this->minZxcvbnScore = config('zxcvbn.min_score') ?? 3;
+    }
+
+    public function passes($attribute, $value)
+    {
+        return $this->zxcvbn
+                ->passwordStrength($value, $this->userInputs)['score'] >= $this->minZxcvbnScore;
+    }
+
+    public function message()
+    {
+        return 'The :attribute must be stronger.';
+    }
+}
