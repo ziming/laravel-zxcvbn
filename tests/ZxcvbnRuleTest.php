15d270173f4a354f7c8d1566fc979875b39923af
ziming <ziming@users.noreply.github.com>
2023-02-11 09:20:57 +0000

    Fix styling

--- tests/ZxcvbnRuleTest.php
+++ tests/ZxcvbnRuleTest.php
@@ -3,13 +3,12 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 use Ziming\LaravelZxcvbn\Rules\ZxcvbnRule;
-use ZxcvbnPhp\Zxcvbn;
 
 it('fails with weak passwords', function () {
     $weakPassword = Arr::random(['zxcvbn', 'qwerty', '12345678']);
 
     $validator = Validator::make(['password' => $weakPassword], [
-        'password' => ['required', new ZxcvbnRule]
+        'password' => ['required', new ZxcvbnRule()],
     ]);
 
     expect($validator->fails())->toBeTrue();
