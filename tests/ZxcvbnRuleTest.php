493991b9c627a6e6502cd8dbf88e5f5ce0059ae9
ziming <ziming.opensource@gmail.com>
2023-02-12 01:51:57 +0800

    add 2nd test case for strong password

--- tests/ZxcvbnRuleTest.php
+++ tests/ZxcvbnRuleTest.php
@@ -2,14 +2,28 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Ziming\LaravelZxcvbn\Rules\ZxcvbnRule;
 
 it('fails with weak passwords', function () {
     $weakPassword = Arr::random(['zxcvbn', 'qwerty', '12345678']);
 
     $validator = Validator::make(['password' => $weakPassword], [
-        'password' => ['required', new ZxcvbnRule()],
+        'password' => ['required', new ZxcvbnRule([], 4)],
     ]);
 
     expect($validator->fails())->toBeTrue();
 });
+
+it('passes with strong passwords', function () {
+
+    // possible to generate a weak one by chance but it should be very rare
+    $strongPassword = Str::random();
+
+    $validator = Validator::make(['password' => $strongPassword], [
+        'password' => ['required', new ZxcvbnRule([], 4)],
+    ]);
+
+    expect($validator->passes())->toBeTrue();
+});
+
